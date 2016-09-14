0b47077c66f8b0e35aa3f2e12b7e3fd89cc96c29
Matt Mobley <mattsmobley@gmail.com>
2016-09-13 19:27:48 -0600

    desktop ready mobile coming

--- js/routes.js
+++ js/routes.js
@@ -4,14 +4,14 @@ angular.module("app")
       .when('/', {
         templateUrl: 'views/main.html'
       })
-      .when('/page2', {
-        templateUrl: 'views/page2.html'
+      .when('/properties', {
+        templateUrl: 'views/properties.html'
       })
-      .when('/page3', {
-        templateUrl: 'views/page3.html'
+      .when('/services', {
+        templateUrl: 'views/services.html'
       })
-      .when('/page4', {
-        templateUrl: 'views/page4.html'
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
       });
 
   });
